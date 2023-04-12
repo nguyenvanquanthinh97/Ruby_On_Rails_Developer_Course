2d414a3008ed19084690ba9935c95665b9937b0f
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2023-04-12 16:43:53 +0700

    add users table and connect has_many and belongs_to relationship with articles

--- rails_6_projects/test_app_6/db/schema.rb
+++ rails_6_projects/test_app_6/db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_10_092335) do
+ActiveRecord::Schema.define(version: 2023_04_12_085858) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
