925271177740111ce00e7bab99e853b43667b697
thinhvqnguyen <thinh.nvq.js220597@gmail.com>
2023-04-13 19:40:04 +0700

    user list add

--- rails_6_projects/alplha-blog/app/controllers/users_controller.rb
+++ rails_6_projects/alplha-blog/app/controllers/users_controller.rb
@@ -3,8 +3,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+  end
+
   def edit
     @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
   end
 
   def update
